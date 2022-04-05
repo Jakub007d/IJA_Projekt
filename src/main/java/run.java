ac01e9ed7e4fd7a9db2a77712e7f6151263bd48e
Jakub007d <56787294+Jakub007d@users.noreply.github.com>
2022-04-05 04:01:18 +0200

    Created window place holder

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -1,4 +1,5 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * trieda run
@@ -10,11 +11,23 @@ public class run {
      */
     public static void main(String[] args) {
         System.out.println("Tu bude onedlho kód :D");
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.CYAN);
+        panel.setBounds(0,0,100,100);
         JFrame frame = new JFrame();
+        JLabel label = new JLabel();
+        JLabel label1 = new JLabel("LOL IM BLUE");
+        label.setText("IJA PROJEKT VERZIA ALPHA 1.0");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setVerticalAlignment(JLabel.NORTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("IJA_Projekt");
         frame.setSize(500,500);
         frame.setVisible(true);
+        panel.add(label1);
+        frame.add(panel);
+        frame.add(label);
+
     }
 }
 
