d522ae5ac841084bdb1c79645ac635bd1a283c78
Terezatvojamama <62217151+Terezatvojamama@users.noreply.github.com>
2022-05-16 02:58:49 +0200

    uprava vykreslovania sprav

--- src/main/java/SDGuiPanel.java
+++ src/main/java/SDGuiPanel.java
@@ -36,6 +36,99 @@ public class SDGuiPanel extends JPanel {
         g.drawLine(x+dir*10, y-dir*10, x-dir*10, y+dir*10);
     }
 
+    /**
+     * vracia text, podla typu spravy.
+     * @param message sprava, ktora sa posiela.
+     * @return text spravy, ktora sa vykresli.
+     */
+    private String messageText(UMLMessage message) {
+        switch (message.getMessageType()) {
+            case CREATE:
+                return "<<create>>"+message.getMessage();
+            case DESTROY:
+                return "<<destroy>>" + message.getMessage();
+            default:
+                return message.getMessage();
+        }
+    }
+
+    /**
+     * nastavi farbu spravy.
+     * @param g
+     * @param message
+     */
+    private void setMessageColor(Graphics g, UMLMessage message) {
+        if (message.getMethodExists()) {
+            g.setColor(Color.black);
+        } else {
+            g.setColor(Color.red);
+        }
+    }
+
+    /**
+     * nastavi typ ciary sprave
+     * @param g
+     * @param message
+     */
+    public void setLineType(Graphics2D g, UMLMessage message) {
+        if (message.getMessageType() == UMLMessage.UMLMessageType.RETURN) {
+            Stroke dashed = new BasicStroke(2, BasicStroke.CAP_BUTT,
+                    BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+            g.setStroke(dashed);
+        } else {
+            g.setStroke(new BasicStroke(2));
+        }
+    }
+
+    private void drawMessage(Graphics g,int index,Component panel, UMLMessage message) {
+        int offset = 40 + index * 40;
+        int x = panel.getX() + panel.getWidth() / 2;
+        int y = panel.getY() + offset;
+        int sender = sequenceDiagram.participantPosition(message.getSender());
+        int recipient = sequenceDiagram.participantPosition(message.getRecipient());
+        int direction = panel.getWidth() * (recipient-sender);
+        int dir = arrowDirection(direction);
+
+        drawMessageLine(g,x,y,direction,dir,message.getMessageType());
+        //nastavi text spravy
+        String messageString = messageText(message);
+        //vypise text
+        int strWidth = g.getFontMetrics().stringWidth(messageString);
+        g.drawString(messageString, x + direction / 2 - strWidth / 2, y - 10);
+        drawMessageArrow(g,x,y,direction,dir,message.getMessageType());
+
+    }
+
+    private void drawMessageLine(Graphics g, int x, int y, int direction, int dir, UMLMessage.UMLMessageType type) {
+        if (type == UMLMessage.UMLMessageType.DESTROY) {
+            g.drawLine(x, y, x + direction + dir*10, y);
+        } else {
+            g.drawLine(x, y, x + direction, y);
+        }
+    }
+
+    private void drawMessageArrow(Graphics g, int x, int y, int direction, int dir, UMLMessage.UMLMessageType type) {
+        switch (type) {
+            case RETURN:
+            case ASYN:
+                drawArrow(g, x+direction, y, dir);
+                break;
+            case DESTROY:
+                drawDestroyArrow(g, x + direction, y, dir);
+                break;
+            default:
+                drawFilledArrow(g, x+direction, y, dir);
+        }
+    }
+
+    private int arrowDirection(int direction) {
+        if (direction > 0) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
     @Override
     public void paint(Graphics g) {
         super.paint(g);
@@ -53,71 +146,22 @@ public class SDGuiPanel extends JPanel {
                     for (UMLMessage message : messageList) {
                         if (Objects.equals(panel.getName(), message.getSender().getName())) {
                             int index = sequenceDiagram.messagePosition(message);
-
                             Graphics2D g2d = (Graphics2D) g.create();
                             //settings for message color
-                            if (message.getMethodExists()) {
-                                g2d.setColor(Color.black);
-                            } else {
-                                g2d.setColor(Color.red);
-                            }
+                            setMessageColor(g2d,message);
 
                             // settings for message line type
-                            switch (message.getMessageType()) {
-                                case RETURN:
-                                    Stroke dashed = new BasicStroke(2, BasicStroke.CAP_BUTT,
-                                            BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
-                                    g2d.setStroke(dashed);
-                                    break;
-                                default:
-                                    g2d.setStroke(new BasicStroke(2));
-                            }
+                            setLineType(g2d,message);
 
-                            int offset = 40 + index * 40;
-                            int x = panel.getX() + panel.getWidth() / 2;
-                            int y = panel.getY() + offset;
-                            int sender = sequenceDiagram.participantPosition(message.getSender());
-                            int recipient = sequenceDiagram.participantPosition(message.getRecipient());
-
-                            //dlzka spravy a smer
-                            int direction = panel.getWidth() * (recipient-sender);
-
-                            //smer spravy
-                            int dir;
-                            if (direction > 0) {
-                                dir = -1;
-                            } else {
-                                dir = 1;
-                            }
-
-                            //nakresli ciaru spojujucu ucastnikov medzi ktorymi je sprava
-                            if (message.getMessageType() == UMLMessage.UMLMessageType.DESTROY) {
-                                g2d.drawLine(x, y, x + direction + dir*10, y);
-                            } else {
-                                g2d.drawLine(x, y, x + direction, y);
-                            }
-
-                            //vypise text
-                            int strWidth = g2d.getFontMetrics().stringWidth(message.getMessage());
-                            g2d.drawString(message.getMessage(),x+direction/2-strWidth/2, y-10);
+                            // nakresli spravu
+                            drawMessage(g2d,index,panel,message);
 
                             // test jlabel
                             JLabel label = new JLabel(message.getMessage());
                             label.setBackground(Color.orange);
                             // test jlabel
 
-                            // typ sipky
-                            switch (message.getMessageType()) {
-                                case RETURN:
-                                case ASYN:
-                                    drawArrow(g2d, x+direction, y, dir);
-                                    break;
-                                case DESTROY:
-                                    drawDestroyArrow(g2d, x + direction, y, dir);
-                                    break;
-                                default:
-                                    drawFilledArrow(g2d, x+direction, y, dir);
-                            }
+
                             // Get rid of the copy
                             g2d.dispose();
                         }
